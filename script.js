1d261fec04f76ca357a787d78ae84443a11632f8
Jiashu Yang <jiashuyang0403@gmail.com>
2022-09-01 20:23:31 -0400

    finished product for sleeptime

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const colors = [
     "rgb(168, 39, 254)",
-            "rgb(154, 3, 254)", "" +
-            "rgb(150, 105, 254)",
-            "rgb(140, 140, 255)",
-        "rgb(187, 187, 255)",
-        "rgb(143, 254, 221)",
+    "rgb(154, 3, 254)",
+    "rgb(150, 105, 254)",
+    "rgb(140, 140, 255)",
+    "rgb(187, 187, 255)",
+    "rgb(143, 254, 221)",
     ]
 
     function createWakeUpTimeElement(time, cycle) {
@@ -20,9 +20,14 @@ const colors = [
         let hours = document.getElementById("hours");
         hours.innerHTML = "";
 
+        let hh = document.getElementById("hh").value;
+        let mm = document.getElementById("mm").value;
+        let ampm = document.getElementById("ampm").value;
+        hh = ampm === "PM" ? hh + 12 : hh;
         let now = new Date();
         // allow 14 minutes to fall sleep!
-        now.setMinutes(now.getMinutes() + 14);
+        now.setHours(hh);
+        now.setMinutes(mm);
 
         // calculate sleep cycles!
         for (let i = 1; i <= 6; i++) {
